40120a104e9fb4a9be1bf3c6f2fbba86d6a7b6e0
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-06-17 14:53:27 +0100

    initialisation

---
+++ main.py
@@ -0,0 +1,2 @@
+# faire le choix
+Veille_selectionner = int(input("Vielles selectionner \n1. Compte M-pesa USD\n2. Compte M-pesa FC\n3. Inviter un proche\n4. Balance Rallonge (gratuit)\n5. Compte Bonus\nChoix : "))
