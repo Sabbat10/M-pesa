1124f0a47b7fa5c4405c499ddcefcbb0443f566f
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-06-20 15:16:48 +0100

    add faireChox function

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
-# faire le choix
-Veille_selectionner = int(input("Vielles selectionner \n1. Compte M-pesa USD\n2. Compte M-pesa FC\n3. Inviter un proche\n4. Balance Rallonge (gratuit)\n5. Compte Bonus\nChoix : "))
+
+def usd():
+    
+    while True:
+        print() 
+        choix_str = input("Vielles selectionner \n1. Compte M-pesa USD\n2. Compte M-pesa FC\n3. Inviter un proche\n4. Balance Rallonge (gratuit)\n5. Compte Bonus\nChoix : ")
+        try:
+            choix = int(choix_str)
+            if choix in [1, 2, 3, 4, 5]:
+                print("Choix valide usd")
+                break
+            else:
+                print("Choix invalide. Veuillez saisir un nombre entre 1 et 5.")
+        except ValueError:
+            print("Saisie invalide. Veuillez saisir un nombre.")
+    
+    return choix
+
+
+def faire_choix():
+    
+    while True:
+        print() 
+        choix_str = input("Vielles selectionner \n1. Compte M-pesa USD\n2. Compte M-pesa FC\n3. Inviter un proche\n4. Balance Rallonge (gratuit)\n5. Compte Bonus\nChoix : ")
+        try:
+            choix = int(choix_str)
+            if choix in [1, 2, 3, 4, 5]:
+                usd()
+                break
+            else:
+                print("Choix invalide. Veuillez saisir un nombre entre 1 et 5.")
+        except ValueError:
+            print("Saisie invalide. Veuillez saisir un nombre.")
+    
+    return choix
+
+faire_choix()
