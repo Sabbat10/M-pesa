a26bcb7593c999d75098567a2927f3c9a6e4c442
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-06-21 12:38:57 +0100

    add currency

---
+++ curency/fc.py
@@ -0,0 +1,16 @@
+def fc():
+    
+    while True:
+        print() 
+        choix_str = input("Compte M-pesa FC \n1. Envoie Argent\n2. Achat Credit ou Forfaits\n3. Retirer du Cash\n4. Services Financiers\n5. Mes Paiements\n6. Bureau de Change\n7. Services Compte\n8. Retour\nChoix :  ")
+        try:
+            choix = int(choix_str)
+            if choix in [1, 2, 3, 4, 5]:
+                print("Choix valide usd")
+                break
+            else:
+                print("Choix invalide. Veuillez saisir un nombre entre 1 et 5.")
+        except ValueError:
+            print("Saisie invalide. Veuillez saisir un nombre.")
+    
+    return choix
--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-
-def usd():
-    
-    while True:
-        print() 
-        choix_str = input("Vielles selectionner \n1. Compte M-pesa USD\n2. Compte M-pesa FC\n3. Inviter un proche\n4. Balance Rallonge (gratuit)\n5. Compte Bonus\nChoix : ")
-        try:
-            choix = int(choix_str)
-            if choix in [1, 2, 3, 4, 5]:
-                print("Choix valide usd")
-                break
-            else:
-                print("Choix invalide. Veuillez saisir un nombre entre 1 et 5.")
-        except ValueError:
-            print("Saisie invalide. Veuillez saisir un nombre.")
-    
-    return choix
-
+from curency.usd import usd
+from curency.fc import fc
 
 def faire_choix():
     
@@ -24,10 +8,23 @@ def faire_choix():
         choix_str = input("Vielles selectionner \n1. Compte M-pesa USD\n2. Compte M-pesa FC\n3. Inviter un proche\n4. Balance Rallonge (gratuit)\n5. Compte Bonus\nChoix : ")
         try:
             choix = int(choix_str)
-            if choix in [1, 2, 3, 4, 5]:
+            if choix == 1:
                 usd()
                 break
+            elif choix == 2:
+                fc()
+                break
+            elif choix == 3:
+                print("choix 3")
+                break
+            elif choix == 4:
+                print("Choix 4")
+                break
+            elif choix == 5:
+                print("Choix 5") 
+                break       
             else:
+                print()
                 print("Choix invalide. Veuillez saisir un nombre entre 1 et 5.")
         except ValueError:
             print("Saisie invalide. Veuillez saisir un nombre.")
